3ee051547a1f060a41e1c7cf2e0c1a7ed232c3b1
Vidar André Bø <56341952+vidarandrebo@users.noreply.github.com>
2023-12-18 17:26:13 +0100

    feat: indication and appearance on menubuttons (#12)

--- src/Client/src/App.tsx
+++ src/Client/src/App.tsx
@@ -1,12 +1,6 @@
 import TopMenu from './components/navigation/TopMenu'
 import BottomMenu from './components/navigation/BottomMenu'
-import {Route, Routes} from "react-router-dom";
-import Login from "./components/pages/Login.tsx";
-import Register from "./components/pages/Register.tsx";
-import Home from "./components/pages/Home.tsx";
-import Settings from "./components/pages/Settings.tsx";
-import MealTracking from "./components/pages/MealTracking.tsx";
-import Food from "./components/pages/Food.tsx";
+import RouteContainer from "./components/navigation/RouteContainer.tsx";
 
 function App() {
     //const [count, setCount] = useState(0)
@@ -15,19 +9,9 @@ function App() {
         <>
             <div className='flex flex-col h-screen'>
                 <TopMenu/>
-                <main className='flex-1 overflow-y-auto'>
-                    <Routes>
-                        <Route path='/' element={<Home></Home>} />
-                        <Route path='/login' element={<Login></Login>} />
-                        <Route path='/register' element={<Register></Register>}/>
-                        <Route path='/settings' element={<Settings></Settings>}/>
-                        <Route path='/meal-tracking' element={<MealTracking></MealTracking>}/>
-                        <Route path='/food' element={<Food></Food>}/>
-                    </Routes>
-                </main>
+                <RouteContainer/>
                 <BottomMenu/>
             </div>
-
         </>
     )
 }
--- src/Client/src/components/navigation/TopMenu.tsx
+++ src/Client/src/components/navigation/TopMenu.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 
 export default function TopMenu() {
     return (<>
-        <header className='h-10 flex justify-between'>
+        <header className='h-10 flex justify-between sticky top-0 z-50 bg-white'>
             <p className='p-1'>Nutrition Tracker</p>
             <nav className='flex justify-end'>
                 <Link to="/login" className='p-1'>Login</Link>
