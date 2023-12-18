9fcd3a227ec3e9477c12d1c2c8c7e2a7ceea972e
vidarandrebo <vidarandre.bo@protonmail.com>
2023-12-18 17:25:11 +0100

    feat: indication and appearance on menubuttons

--- src/Client/src/components/pages/Home.tsx
+++ src/Client/src/components/pages/Home.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <h1>Home Page</h1>
+    return <>
+        <div className="h-96 bg-blue-600"></div>
+        <div className="h-96 bg-green-400"></div>
+        <div className="h-96 bg-red-400"></div>
+    </>
 }
