76600ffb7244052be5f783177b1ce9b490b89e6a
Vidar André Bø <vidarandre.bo@protonmail.com>
2025-07-23 20:22:43 +0200

    feat: buttons for deleting meal entries

--- client/src/Pages/HomePage/MealView.vue
+++ client/src/Pages/HomePage/MealView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import ButtonDanger from "../../Components/Buttons/ButtonDanger.vue";
 import type { MealView } from "../../Models/Meals/MealView.ts";
-import { onBeforeUnmount, onMounted, ref, useTemplateRef } from "vue";
+import { ref } from "vue";
+import { OnClickOutside } from "@vueuse/components";
 import LevelPrimary from "../../Components/LevelPrimary.vue";
+import MealEntryView from "./MealEntryView.vue";
 
-import { v4 as uuidv4 } from "uuid";
-const el = useTemplateRef("p");
-const dropDownId = uuidv4();
 const props = defineProps<{
     item: MealView;
 }>();
@@ -16,21 +15,10 @@ const emit = defineEmits<{
 }>();
 
 const kebabOpen = ref<boolean>(false);
-const mouseFunc = (e: MouseEvent) => {
-    if (e.target) {
-        if (el.value?.contains(e.target)) {
-            if (kebabOpen.value) {
-                kebabOpen.value = false;
-            }
-        }
-    }
-};
-onMounted(() => {
-    window.addEventListener("click", mouseFunc);
-});
-onBeforeUnmount(() => {
-    window.removeEventListener("click", mouseFunc);
-});
+
+function onClickOutsideHandler() {
+    kebabOpen.value = false;
+}
 </script>
 
 <template>
@@ -41,8 +29,8 @@ onBeforeUnmount(() => {
             </div>
         </template>
         <template #right>
-            <div ref="p">
-                <div :class="kebabOpen ? 'dropdown is-active is-hoverable is-right' : 'dropdown is-hoverable is-right'">
+            <OnClickOutside @trigger="onClickOutsideHandler">
+                <div :class="kebabOpen ? 'dropdown is-active is-right' : 'dropdown is-right'">
                     <div class="dropdown-trigger">
                         <button
                             class="button"
@@ -53,15 +41,15 @@ onBeforeUnmount(() => {
                             <span>⋮</span>
                         </button>
                     </div>
-                    <div :id="dropDownId" class="dropdown-menu" role="menu">
+                    <div class="dropdown-menu" role="menu">
                         <div class="dropdown-content">
                             <a href="#" class="dropdown-item" @click="() => emit('deleteMeal', props.item.id)"
-                                >Delete</a
+                                >Delete meal</a
                             >
                         </div>
                     </div>
                 </div>
-            </div>
+            </OnClickOutside>
         </template>
     </LevelPrimary>
     <div>
@@ -69,12 +57,10 @@ onBeforeUnmount(() => {
     </div>
     <ul class="content">
         <li v-for="entry in props.item.entries" :key="entry.id" class="box">
-            <p>{{ entry.name }}, {{ entry.amount }}</p>
-            <p>
-                KCal: {{ entry.KCal }}, Protein: {{ entry.Protein }}, Carbohydrate: {{ entry.Carbohydrate }}, Fat:
-                {{ entry.Fat }}
-            </p>
-            <ButtonDanger @click="() => emit('deleteMealEntry', entry.id, props.item.id)">Delete</ButtonDanger>
+            <MealEntryView
+                :entry="entry"
+                @deleteMealEntry="(entryId) => emit('deleteMealEntry', entryId, props.item.id)"
+            ></MealEntryView>
         </li>
     </ul>
 </template>
